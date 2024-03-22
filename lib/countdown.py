e2e11072a7b12e535a25cede0d0e4b7b68b639c5
heather-tech <heather.k.tech@gmail.com>
2024-03-22 14:44:19 -0400

    completed
    all tests passing

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,19 @@
 # your code goes here!
+
+
+def countdown(x):
+    while x:
+        print(f'{x} SECOND(S)!')
+        x -= 1
+
+    print("HAPPY NEW YEAR!")
+
+def countdown_with_sleep(x):
+    import time
+    time.sleep(1)
+    while x:
+        print(f'{x} SECOND(S)!')
+        x -= 1
+
+    print("HAPPY NEW YEAR!")
+
